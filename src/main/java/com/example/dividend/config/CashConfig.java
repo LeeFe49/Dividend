ed3181c601b830848cd874e3bc16f5dc378a0d93
LeeFe49 <chullee49@naver.com>
2024-02-06 22:16:10 +0900

    refactor : redis 기본 설정

---
+++ src/main/java/com/example/dividend/config/CashConfig.java
@@ -0,0 +1,11 @@
+package com.example.dividend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class CashConfig {
+
+    
+}
